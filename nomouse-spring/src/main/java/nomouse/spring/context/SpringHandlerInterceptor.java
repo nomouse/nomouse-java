9ca02cfa2b35c86668d3b64001c4a60791a9a7cc
nomouse <nomousewch@gmail.com>
2015-01-12 17:57:38 +0800

    1

--- nomouse-spring/src/main/java/nomouse/spring/context/SpringHandlerInterceptor.java
+++ nomouse-spring/src/main/java/nomouse/spring/context/SpringHandlerInterceptor.java
@@ -1,5 +1,6 @@
 package nomouse.spring.context;
 
+import nomouse.spring.controller.param.Req;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,10 @@ public class SpringHandlerInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         System.out.println(request.getContextPath());
+        request.getHeaderNames();
+
+        Req req = new Req();
+        request.setAttribute("req", req);
 
         return super.preHandle(request, response, handler);
 
---
+++ nomouse-spring/src/main/java/nomouse/spring/controller/param/Req.java
@@ -0,0 +1,7 @@
+package nomouse.spring.controller.param;
+
+/**
+ * Created by nomouse on 2015/1/12.
+ */
+public class Req {
+}
