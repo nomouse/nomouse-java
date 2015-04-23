47e46578adfecf0c9fa8cae245975bf386123ec5
nomouse <nomouse@163.com>
2015-04-23 13:43:08 +0800

    项目重构

--- nomouse-core/src/main/java/nomouse/core/context/SpringContext.java
+++ nomouse-core/src/main/java/nomouse/core/context/SpringContext.java
@@ -13,7 +13,7 @@ import org.springframework.context.ApplicationContextAware;
 /**
  * 以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候中取出ApplicaitonContext.
  */
-public class SpringContextAware implements ApplicationContextAware {
+public class SpringContext implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
@@ -21,7 +21,7 @@ public class SpringContextAware implements ApplicationContextAware {
      * 实现ApplicationContextAware接口的context注入函数, 将其存入静态变量.
      */
     public void setApplicationContext(ApplicationContext applicationContext) {
-        SpringContextAware.applicationContext = applicationContext; // NOSONAR
+        SpringContext.applicationContext = applicationContext; // NOSONAR
     }
 
     /**
--- nomouse-spring/src/main/java/nomouse/spring/api/param/UserParam.java
+++
@@ -1,7 +0,0 @@
-package nomouse.spring.api.param;
-
-/**
- * Created by nomouse on 2015/3/19.
- */
-public class UserParam {
-}
